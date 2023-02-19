4344d525e0e8e7f0447dc21be53fe39d1327b0e5
devflamenbaum <bielflam.engpet@gmail.com>
2023-02-18 23:42:39 -0300

    create endpoint delete customer

--- src/main/java/com/flamenbaum/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/flamenbaum/hexagonal/adapters/in/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.flamenbaum.hexagonal.adapters.in.controller;
 import com.flamenbaum.hexagonal.adapters.in.controller.mapper.CustomerMapper;
 import com.flamenbaum.hexagonal.adapters.in.controller.request.CustomerRequest;
 import com.flamenbaum.hexagonal.adapters.in.controller.response.CustomerResponse;
+import com.flamenbaum.hexagonal.application.ports.in.DeleteCustomerByIdInputPort;
 import com.flamenbaum.hexagonal.application.ports.in.FindCustomerByIdInputPort;
 import com.flamenbaum.hexagonal.application.ports.in.InsertCustomerInputPort;
 import com.flamenbaum.hexagonal.application.ports.in.UpdateCustomerInputPort;
@@ -23,6 +24,9 @@ public class CustomerController {
 
     @Autowired
     private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
+    private DeleteCustomerByIdInputPort deleteCustomerByIdInputPort;
 
     @Autowired
     private CustomerMapper customerMapper;
@@ -47,6 +51,12 @@ public class CustomerController {
         var customer = customerMapper.toCostumer(customerRequest);
         customer.setId(id);
         updateCustomerInputPort.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") final String id){
+        deleteCustomerByIdInputPort.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/flamenbaum/hexagonal/application/ports/in/DeleteCustomerByIdInputPort.java
@@ -0,0 +1,6 @@
+package com.flamenbaum.hexagonal.application.ports.in;
+
+public interface DeleteCustomerByIdInputPort {
+
+    void delete(String id);
+}
