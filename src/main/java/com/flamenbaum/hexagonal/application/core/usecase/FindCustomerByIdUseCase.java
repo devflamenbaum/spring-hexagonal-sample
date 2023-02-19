c330c8a6b137d99fca27854f2592ea07710b80e8
devflamenbaum <bielflam.engpet@gmail.com>
2023-02-18 22:35:47 -0300

    Create find Customer by id use case

--- src/main/java/com/flamenbaum/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
+++ src/main/java/com/flamenbaum/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
@@ -1,4 +1,19 @@
 package com.flamenbaum.hexagonal.application.core.usecase;
 
+import com.flamenbaum.hexagonal.application.core.domain.Customer;
+import com.flamenbaum.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+
+import java.util.Optional;
+
 public class FindCustomerByIdUseCase {
+
+    private final FindCustomerByIdOutputPort findCustomerByIdOutputPort;
+
+    public FindCustomerByIdUseCase(FindCustomerByIdOutputPort findCustomerByIdOutputPort) {
+        this.findCustomerByIdOutputPort = findCustomerByIdOutputPort;
+    }
+
+    public Customer find(String id){
+        return findCustomerByIdOutputPort.find(id).orElseThrow(RuntimeException::new);
+    }
 }
---
+++ src/main/java/com/flamenbaum/hexagonal/application/ports/out/FindCustomerByIdOutputPort.java
@@ -0,0 +1,10 @@
+package com.flamenbaum.hexagonal.application.ports.out;
+
+import com.flamenbaum.hexagonal.application.core.domain.Customer;
+
+import java.util.Optional;
+
+public interface FindCustomerByIdOutputPort {
+
+    Optional<Customer> find(String id);
+}
